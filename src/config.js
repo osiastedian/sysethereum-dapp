59a1b88ed1e5538fa89ee7656b9377a403397dca
Ted Ian Osias <ted@syscoin.org>
2024-09-03 22:02:56 +0800

    testnet config

--- src/config.js
+++ src/config.js
@@ -5,20 +5,20 @@ class CONFIGURATION {
     this.RelayContract = "0xD822557aC2F2b77A1988617308e4A29A89Cb95A6";
     this.ERC20Manager = "0xA738a563F9ecb55e0b2245D1e9E380f0fE455ea1";
     this.TokenFreezeFunction = "0x7ca654cf9212e4c3cf0164a529dd6159fc71113f867d0b09fdeb10aa65780732";
-    this.NEVMExplorerURL = "https://explorer.syscoin.org/";
+    this.NEVMExplorerURL = "https://tanenbaum.io/";
     this.NEVMAddressExplorerURL = this.NEVMExplorerURL + "address/";
-    this.SyscoinTxExplorerURL = "https://blockbook.syscoin.org/tx/";
+    this.SyscoinTxExplorerURL = "https://blockbook-dev.syscoin.org/tx/";
     this.NEVMTxExplorerURL = this.NEVMExplorerURL + "tx/";
     this.FaucetURL = "https://faucet.syscoin.org/";
-    this.BlockbookAPIURL = "https://blockbook.syscoin.org/";
-    this.ChainId = "0x39"; // 5700 testnet, 57 mainnet
+    this.BlockbookAPIURL = "https://blockbook-dev.syscoin.org/";
+    this.ChainId = "0x1644";// "0x39"; // 5700 testnet, 57 mainnet
     this.ChainName = "Syscoin NEVM";
     this.NativeCurrencyName = "Syscoin";
-    this.NativeCurrencySymbol = "SYS";
+    this.NativeCurrencySymbol = "TSYS";
     this.SYSXAsset = "123456";
     this.TokenUnfreezeFn = '0x8561b3bd1c885cb9cea8df62881a35fe7335336cd5450fd96d14176100c4657d';
     this.SysNetwork = sjs.utils.syscoinNetworks.mainnet;
-    this.EasySwapAPI = "http://localhost:8080/api/"
+    this.EasySwapAPI = "/api/"
   }
 }
 export default new CONFIGURATION();
